9318f041a8a0c28005da00caba6bb3f0f011c607
DESKTOP-94R8IIJ\user <jinhokim@micehub.com>
2024-04-05 17:56:33 +0900

    Add main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,29 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 export default function Page() {
-  return <>Home</>;
+  const router = useRouter();
+
+  // console.log('router', router);
+
+  return (
+    <>
+      <h1>Home</h1>
+      <button
+        onClick={() => {
+          router.push('/basic');
+        }}
+      >
+        <h3>Basic page</h3>
+      </button>
+      <button
+        onClick={() => {
+          router.push('/usage');
+        }}
+      >
+        <h3>Usage page</h3>
+      </button>
+    </>
+  );
 }
