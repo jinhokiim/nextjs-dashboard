eda89e7c3786d3d05563c33eb46e2e4033d80504
DESKTOP-94R8IIJ\user <jinhokim@micehub.com>
2024-04-05 17:56:48 +0900

    Add basic page with react query

--- app/basic/components/BasicPageTest.tsx
+++ app/basic/components/BasicPageTest.tsx
@@ -1,5 +1,35 @@
 'use client';
+import axios from 'axios';
+
+import { useQuery } from '@tanstack/react-query';
 
 export default function BasicPageTest() {
-  return <>BasicPageTest</>;
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      axios
+        .get('https://api.github.com/repos/tannerlinsley/react-query')
+        .then((res) => res.data),
+  });
+
+  return (
+    <>
+      <div>
+        <h1>Basic page</h1>
+        {!isFetching && (
+          <>
+            <h1>name: {data.name}</h1>
+            <p>description: {data.description}</p>
+            <strong>subscribers_count 👀: {data.subscribers_count}</strong>
+            {' | '}
+            <strong>stargazers_count ✨: {data.stargazers_count}</strong>
+            {' | '}
+            <strong>forks_count 🍴: {data.forks_count}</strong>
+          </>
+        )}
+
+        <div>{isFetching ? 'Updating...' : ''}</div>
+      </div>
+    </>
+  );
 }
